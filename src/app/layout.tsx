fe9a3cf98510c82d7f2c2426f0ddc079f9209d0f
ramy sabry <ramyalsabry.dev@gmail.com>
2024-03-12 13:49:37 +0200

    pull data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,26 @@ import GlobalLayout from "@/components/GlobalLayout";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  generator: "Next.js",
+  applicationName: "Jobster",
+  referrer: "origin-when-cross-origin",
+  keywords: ["Next.js", "React", "JavaScript", "job", "React.js"],
+  authors: [
+    { name: "Ramy" },
+    { name: "Ramy", url: "https://www.linkedin.com/in/ramy5/" },
+  ],
+  creator: "Ramy Sabry",
   title: "Jobster",
   description: "Post a job or view any job",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,13 +1,15 @@
+"use client";
+
 export const addToLocalStorage = (user) => {
-  localStorage.setItem("user", JSON.stringify(user));
+  window.localStorage.setItem("user", JSON.stringify(user));
 };
 
 export const getFromLocalStorage = () => {
-  const user = localStorage.getItem("user");
+  const user = window.localStorage.getItem("user");
   const userResult = user ? JSON.parse(user) : null;
   return userResult;
 };
 
 export const clearFromLocalStorage = () => {
-  localStorage.removeItem("user");
+  window.localStorage.removeItem("user");
 };
